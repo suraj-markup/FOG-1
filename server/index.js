0a8ce35fb964ba9460490417e14fdcf2830e8bab
suraj-markup <sk9261712674@gmail.com>
2025-01-05 16:07:46 +0530

    Backend done

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const app=express();
+const cors=require("cors");
+const port =5000;
+const serverInfoData=require("./serverInfoData");
+
+app.use(cors(
+    {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+      }
+));
+
+app.get('/',(req,res)=>{
+    res.send("Your server is up. you can access the APIs at /server-info");
+})
+
+app.get('/server-info',(req,res)=>{
+    res.json(serverInfoData);
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
