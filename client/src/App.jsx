6c5ba65389e4abf00be1972c71d41e0251597e80
suraj-markup <sk9261712674@gmail.com>
2025-01-05 17:19:39 +0530

    started frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="bg-[url('./images/menu_background.png')]">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind CSS is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
